c4922af2dcac43d3b78ea1ae7fa8a404324b4427
MrManHD <57962465+MrManHD@users.noreply.github.com>
2023-06-24 13:46:24 +0200

    add new tebex module

---
+++ simplecloud-modules/simplecloud-module-tebex/src/main/kotlin/eu/thesimplecloud/module/tebex/manager/TebexModule.kt
@@ -0,0 +1,18 @@
+package eu.thesimplecloud.module.tebex.manager
+
+import eu.thesimplecloud.api.external.ICloudModule
+
+/**
+ * Created by MrManHD
+ * Class create at 24.06.2023 12:56
+ */
+
+class TebexModule : ICloudModule {
+    override fun onEnable() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDisable() {
+        TODO("Not yet implemented")
+    }
+}
